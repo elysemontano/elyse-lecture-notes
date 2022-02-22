0673e1287e1d36589317e2a587875a1667d4709a
Elyse Montano <elysemontano.dev.com>
2022-02-22 12:29:08 -0800

    updates lecture notes

--- functions-loops-arrays.js
+++ functions-loops-arrays.js
@@ -1,11 +1,18 @@
 // Functions, Loops, and Arrays
 
+// --- NAME FILE: javascript-functions-loops-arrays.js
+// --- BRANCH: javascript-functions-loops-arrays
+
 // Up to this point we have used functions to perform if else logic, but we want to be able to create more complex functions.  So today we are going to discuss tying together functions, loops and arrays to allow complex, reusable, and dynamic code.
 
-// We are going to start with an array of numbers and then multiply each number in the array by 5.
 // EX:
 // Multiply each number in an array of numbers by 5.
+
+// We are going to start with an array of numbers and then multiply each number in the array by 5.
 var myArrayOfNums = [2, 3, 4, 5, 6]
+
+
+// Output: [10, 15, 20, 25, 30]
 
 // When you are faced with a problem that states "I need to do something to each item" or make a decision about each item, think iteration.
 // ITERATION is the process of performing an action over and over until a condition is met.  
@@ -15,7 +22,7 @@ var myArrayOfNums = [2, 3, 4, 5, 6]
 
 // }
 
-// When setting up the array, I will want to accept an array as a parameter.  To keep this reusable and generic though, I can pass a placeholder for the array.  To be clear and descriptive, I am using the word array as my placeholder so I know what data type I am working with
+// When setting up the function, I will want to accept an array as a parameter.  To keep this reusable and generic though, I can pass a placeholder for the array.  To be clear and descriptive, I am using the word array as my placeholder so I know what data type I am working with
 
 // const mult5 = (array) => {
 
@@ -33,28 +40,9 @@ var myArrayOfNums = [2, 3, 4, 5, 6]
 
 // Let's set up this for loop by first finding where we want to start, where we need to end our iteration and how we will increment.
 
-// While for loops don't inately connect with arrays, we can match up our for loop to our index so the index is iterated much like we did last week.  If we have the index of the element in the array, we also have the ability to extract the value.  So let's just console log our index or i for this for loop:
-const mult5 = (array) => {
-  for(let i=0; i<array.length; i++){
-    console.log(i)
-  }
-}
-console.log(mult5(myArrayOfNums))
-// Output:
-// 0
-// 1
-// 2
-// 3
-// 4
-// undefined
+// While for loops don't inately connect with arrays, we can match up our for loop to our index so the index is iterated much like we did last week.  If we have the index of the element in the array, we also have the ability to extract the value. 
 
-// We get back undefined on the last line because we are not returning anything from the function, only console logging values as we iterate.  We can see that our iteration is logging a start value of 0 and taking us through the length of the array.
-// By using the word array as our placeholder, we are not hardcoding a specific array, so we can also run this same function on another array:
-console.log(mult5([3, 4, 5]))
-// We are still getting undefined because we are still not returning something from the function, but our console log now only shows 3 iterations.
-
-
-// Now that we have a series of index, lets use those to get the values of the array.  How do we access a value at a particular index in an array?
+// How do we access a value at a particular index in an array?
 // array[0]
 // But since we want this to be in parallel with our for loop we can have i be our index value.
 // array[i] will give us the value of each iteration
@@ -72,6 +60,8 @@ console.log(mult5(myArrayOfNums))
 // 5
 // 6
 // undefined
+
+// We get back undefined because we are not returning anything from the function, only console logging values as we iterate.  
 
 // Now that we know we are accessing each value of the array, we can use * 5 to multiply each value by 5
 
@@ -99,7 +89,8 @@ const mult5 = (array) => {
 console.log(mult5(myArrayOfNums))
 // --> 10
 
-// So we got back a value that is not undefined, but we only have one value and we wanted to return each number multiplied.  Return is the output of the function, however once the function returns something, it's work is finished and it will not continue.  
+// So we got back a value that is not undefined, but we only have one value and we wanted to return each number multiplied. 
+// Return is the output of the function, however once the function returns something, it's work is finished and it will not continue.  
 
 // We can't return in the loop since that will end our iteration, so we need to store these values somewhere and return that once we have finished our for loop.
 
@@ -127,9 +118,12 @@ const mult5 = (array) => {
 console.log(mult5(myArrayOfNums))
 // Output: [ 10, 15, 20, 25, 30 ]
 
-//There we go!  We were able to take in an array, manipulate it, and return a new array.  We can continue using this dynamicallt as well by passing a different array:
+//There we go!  We were able to take in an array, manipulate it, and return a new array.  We can continue using this dynamically as well by passing a different array:
 console.log(mult5([30, 40, 50]))
 // Output: [150, 200, 250]
+
+// By using the word array as our placeholder, we are not hardcoding a specific array, so we can also run this same function on another array:
+console.log(mult5([3, 4, 5]))
 
 
 
@@ -152,8 +146,6 @@ var teas = ["chai", "earl gray", "green", "herbal"]
 // access each item in the array
 // modify the string of each item
 // return a new array
-
-// By taking an arguement and specifying a placeholder to access inside our function, we make this dynamic and we can use it for multiple different variables with arrays (keep it generic without specifying the variable, just descriptive to the data type)
 
 const coffeeOrder = (array) => {
 	let orders = [] // ----> ADD THIS AFTER PROMPT!
